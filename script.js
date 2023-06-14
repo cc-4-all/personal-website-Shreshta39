9a6bb0be85dbe5be3c80c4f41b8bd0d419df5112
Shreshta39 <136475085+Shreshta39@users.noreply.github.com>
2023-06-14 15:41:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var img = document.getElementById("gallery-img");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
+
+// if you have more images than me, type their names in here.....
+var images = ["picgirl.jpg, animal.jpg, plant.jpg, book.jpg, books.jpg"]
+
+var currentimage = 0;
+
+function changeimage(e) {
+    if (e.target.id == "next" && currentimage !== images.length -1) {
+        // increment the images (in other words, go to the next one)
+        currentimage = currentimage +1;
+    }
+
+    if (e.target.id == "prev" && currentimage  !== 0) {
+        // logic goes in here
+        currentimage = currentimage -1
+    }
+
+    img.setAttribute("src", "images/" + images[currentimages]);
+}
+
+prev.addEventListener("click", changeimage);
+next.addEventListener("click", changeimage);
